69d38427222d6a5256baa10d1f25dbd107b31d1f
filipevolz <filipevolz00@gamil.com>
2025-03-17 21:25:13 -0300

    Login styles ready

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,67 @@
-import { TitlePage } from '@/components/TitlePage'
+import { Label } from '@radix-ui/react-label'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
+import { TitlePage } from '@/components/TitlePage'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          },
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
+  }
+
   return (
     <>
-      <TitlePage title="SignIn" />
-      <h1>SignIn</h1>
+      <TitlePage title="Login" />
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessa painel
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Acompanhar suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <Button className="bg-primary w-full" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
